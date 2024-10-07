48f87e15200447e3f095ab0cb98ff697a2b2595b
riqi3dev <riki.iiidev@gmail.com>
2024-10-07 23:19:52 +0800

    hotfix layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,8 +6,8 @@
       <div class="header">
         <h4>written by Enrique on 6/10/2024</h4>
         <div class="actions">
-          <span class="material-symbols-outlined"> delete </span>
-          <span class="material-symbols-outlined"> edit </span>
+          <span class="material-symbols-outlined action-icon red"> delete </span>
+          <span class="material-symbols-outlined action-icon green"> bookmark </span>
         </div>
       </div>
       <div class="body">
@@ -19,13 +19,34 @@
 </template>
 
 <style lang="scss" scoped>
+.red {
+  color: red;
+}
+
+.green {
+  color: green;
+}
+
 .header {
   display: flex;
+  border-radius: 0.5rem 0.5rem 0 0;
   justify-content: space-between;
   align-items: center;
+  background-color: blue;
+  padding: 1rem;
 }
 .wrapper {
+  border-radius: 0.5rem;
   width: 75%;
   background-color: red;
+}
+.body {
+  padding: 1rem;
+}
+.action-icon {
+  background-color: white;
+  padding: 0.5rem;
+  border-radius: 50%;
+  margin: 0.5rem;
 }
 </style>
